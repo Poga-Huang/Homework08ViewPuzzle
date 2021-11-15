c6f103d7f7eb7d0be54d0902dcce00588d267f41
Poga Huang <pgh86812@gmail.com>
2021-11-15 20:22:44 +0800

    Initial Commit

---
+++ Homework08ViewPuzzle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework08ViewPuzzle
+//
+//  Created by 黃柏嘉 on 2021/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
